73205cb77e3d639fa2c0b44accbe756f8e31dfe9
Cristhian Saavedra <45241687+cristhiansaavedra96@users.noreply.github.com>
2024-09-17 18:45:47 -0300

    Se termina el CRUD de recipes

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -26,14 +26,54 @@ const getRecipeById = async (req, res) => {
 const createRecipe = async (req, res) => {
     try {
         let recipe = {
-            name: req.body.name,
-            description: req.body.description,
-            category: req.body.category
+            titulo: req.body.titulo,
+            preparacion: req.body.preparacion,
+            categoria: req.body.categoria,
+            ingredientes: req.body.ingredientes
+        }
+        let result = await recipesService.createRecipe(recipe);
+        if (result.affectedRows === 0) {
+            res.status(400).send({ message: 'No se pudo crear la receta' });
+            return;
         }
         res.send({
             message: 'Recipe created',
-            recipe: recipe
+            recipeId: result.insertId
         });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+const updateRecipe = async (req, res) => {
+    try {
+        let recipe = {
+            id: req.body.id,
+            titulo: req.body.titulo,
+            preparacion: req.body.preparacion,
+            categoria: req.body.categoria,
+            ingredientes: req.body.ingredientes
+        }
+        let result = await recipesService.updateRecipe(recipe);
+        if (result.affectedRows === 0) {
+            res.status(400).send({ message: 'No se pudo actualizar la receta' });
+            return;
+        }
+        res.send({ message: 'Recipe updated' });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+const deleteRecipe = async (req, res) => {
+    try {
+        let recipeId = req.body.id;
+        let result = await recipesService.deleteRecipe(recipeId);
+        if (result.affectedRows === 0) {
+            res.status(400).send({ message: 'No se pudo eliminar la receta' });
+            return;
+        }
+        res.send({ message: 'Recipe deleted' });
     } catch (error) {
         res.status(500).send(error.message);
     }
@@ -42,7 +82,9 @@ const createRecipe = async (req, res) => {
 const recipesController = {
     getAllRecipes,
     getRecipeById,
-    createRecipe
+    createRecipe,
+    updateRecipe,
+    deleteRecipe
 }
 
 export default recipesController;
---
+++ src/routes/user.js
@@ -0,0 +1,2 @@
+// User va a tener
+// nombre, telefono, email, password, usuario, id
